2c7813f4d4bd083bcb39860ce9456489524bf30e
Powerlax <raovishruth@gmail.com>
2024-09-17 18:00:08 -0700

    bitwise opperations

---
+++ bit_inversions.cpp
@@ -0,0 +1,20 @@
+//
+// Created by innav_z3e3dq9 on 7/10/2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string z;
+    getline(cin, z);
+    int n = z.size();
+    int m;
+    cin >> m;
+    set<int> splits;
+    int ans = 0;
+    for (int i = 0; i<m; i++) {
+
+    }
+    return 0;
+}
