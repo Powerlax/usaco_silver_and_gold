bcc0ec8e60e1f7de152ada159e937c5f3704430e
Powerlax <raovishruth@gmail.com>
2024-10-01 20:45:02 -0700

    update

--- grid_paths.cpp
+++ grid_paths.cpp
@@ -2,3 +2,36 @@
 // Created by innav_z3e3dq9 on 9/30/2024.
 //
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<string> grid(n);
+    for (int i = 0; i < n; i++) {
+        cin >> grid[i];
+    }
+    vector dp(n, vector<int>(n));
+    dp[0][0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == '*') {
+                dp[i][j] = 0;
+            } else {
+                if (i - 1 >= 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+                dp[i][j] %= 1000000007;
+            }
+        }
+    }
+    cout << dp[n-1][n-1] << endl;
+    return 0;
+}
