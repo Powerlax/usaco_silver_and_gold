b90b931b916066f8b715f14e8108410c7ab30fb5
Powerlax <raovishruth@gmail.com>
2024-10-01 21:21:25 -0700

    update

---
+++ cow_camp.cpp
@@ -0,0 +1,20 @@
+//
+// Created by innav_z3e3dq9 on 9/8/2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t, k;
+    cin >> t >> k;
+    double ans = 0.0;
+    for (int i = 0; i < t; i++) {
+
+    }
+    cout << fixed << setprecision(6) << ans << endl;
+    return 0;
+}
