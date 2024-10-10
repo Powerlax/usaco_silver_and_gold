d448291c629bd19a066b0efa1fa21e68248097a0
Powerlax <raovishruth@gmail.com>
2024-10-09 20:39:28 -0700

    update

---
+++ diamond_collector.cpp
@@ -0,0 +1,14 @@
+//
+// Created by innav_z3e3dq9 on 7/9/2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("diamonds.in", "r", stdin);
+    freopen("diamonds.out", "w", stdout);
+    int n;
+
+    return 0;
+}
