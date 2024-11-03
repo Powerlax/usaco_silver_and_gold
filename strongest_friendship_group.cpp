7dc7d4d53300ceeabc79df33def1db67fbe7dd9c
Powerlax <raovishruth@gmail.com>
2024-11-03 13:27:25 -0800

    update

--- strongest_friendship_group.cpp
+++ strongest_friendship_group.cpp
@@ -1,3 +1,14 @@
 //
 // Created by innav_z3e3dq9 on 11/3/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    return 0;
+}
