a38d826609a0162080b44ad4a00c6112044414e8
Powerlax <raovishruth@gmail.com>
2024-11-03 13:27:32 -0800

    update

---
+++ packing_regulations.cpp
@@ -0,0 +1,24 @@
+//
+// Created by innav_z3e3dq9 on 7/15/2024.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        set<int> claimed;
+        for (int i = 0; i<n; i++) {
+            int a, b;
+            cin >> a >> b;
+        }
+    }
+    return 0;
+}
