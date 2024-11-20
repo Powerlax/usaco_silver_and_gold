e2097510251526a9e2c3531bc57fc6f08db14868
Powerlax <raovishruth@gmail.com>
2024-11-19 18:27:14 -0800

    update

--- milking_order.cpp
+++ milking_order.cpp
@@ -1,3 +1,15 @@
 //
 // Created by innav_z3e3dq9 on 11/16/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    freopen("milkorder.in", "r", stdin);
+    freopen("milkorder.out", "w", stdout);
+    return 0;
+}
