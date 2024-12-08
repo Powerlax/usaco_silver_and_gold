72b7e50e5ff82d16e7184ea40f474ccaebf017ea
Powerlax <raovishruth@gmail.com>
2024-12-08 10:31:08 -0800

    update

--- distance_queries.cpp
+++ distance_queries.cpp
@@ -1,3 +1,26 @@
 //
 // Created by innav_z3e3dq9 on 12/5/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n, q;
+    cin >> n >> q;
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n - 1; i++) {
+        const auto a = new int[n];
+        const auto b = new int[n];
+        free(a);
+        free(b);
+    }
+    while (q--) {
+        int a, b;
+        cin >> a >> b;
+    }
+    return 0;
+}
