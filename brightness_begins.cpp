5bd937939f8775ef2499c92252e1dade27ed89c2
Powerlax <raovishruth@gmail.com>
2024-12-23 15:06:43 -0800

    update

--- brightness_begins.cpp
+++ brightness_begins.cpp
@@ -1,3 +1,20 @@
 //
 // Created by innav_z3e3dq9 on 12/19/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        long long k;
+        cin >> k;
+        cout << k + static_cast<int>(sqrtl(k) + 0.5) << endl;
+    }
+    return 0;
+}
