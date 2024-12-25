e66b5838405223a8e5691b81c06da2742cfa13d7
Powerlax <raovishruth@gmail.com>
2024-12-24 18:21:55 -0800

    update

--- fenced_in.cpp
+++ fenced_in.cpp
@@ -1,6 +1,7 @@
 //
 // Created by innav_z3e3dq9 on 11/22/2024.
 //
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,68 +11,92 @@ struct Edge {
 	int j;
 };
 
+bool comp(const Edge &t, const Edge &y) {
+	return t.cost < y.cost;
+}
+
 class DisjointSets {
 	vector<int> parents;
 	vector<int> sizes;
-	explicit DisjointSets(const int n) : parents(n, -1), sizes(n) {}
 
-	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+public:
+	explicit DisjointSets(const int n) : parents(n, -1), sizes(n, 1) {}
 
-	bool same_set(int a, int b) { return get(a) == get(b); }
+	int find(const int x) {
+		if (parents[x] == x || parents[x] == -1) {
+			return x;
+		}
+		return parents[x] = find(parents[x]);
+	}
 
-	int size(int x) { return -e[get(x)]; }
-
-	bool unite(int x, int y) {  // union by size
-		x = get(x), y = get(y);
-		if (x == y) return false;
-		if (e[x] > e[y]) swap(x, y);
-		e[x] += e[y];
-		e[y] = x;
+	bool unite(const int x, const int y) {
+		int x_root = find(x);
+		int y_root = find(y);
+		if (x_root == y_root) {
+			return false;
+		}
+		if (sizes[x_root] < sizes[y_root]) {
+			swap(x_root, y_root);
+		}
+		sizes[x] += sizes[y];
+		parents[y] = x;
 		return true;
+	}
+
+	bool connected(const int a, const int b) {
+		return find(a) == find(b);
 	}
 };
 
 int main() {
-	freopen("fencedin.in", "r", stdin);
-	freopen("fencedin.out", "w", stdout);
-	int hdist, vdist, n, m;
-	cin >> hdist >> vdist >> n >> m;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	//freopen("fencedin.in", "r", stdin);
+	//freopen("fencedin.out", "w", stdout);
+	int a, b, n, m;
+	cin >> a >> b >> n >> m;
 	vector<int> vx(n + 1), vy(m + 1);
-	for (int i = 1; i <= n; i++) { cin >> vx[i]; }
-	for (int i = 1; i <= m; i++) { cin >> vy[i]; }
-	vx.push_back(hdist);
-	vy.push_back(vdist);
+	for (int i = 1; i <= n; i++) {
+		cin >> vx[i];
+	}
+	for (int i = 1; i <= m; i++) {
+		cin >> vy[i];
+	}
+	vx.push_back(a);
+	vy.push_back(b);
 	sort(vx.begin(), vx.end());
 	sort(vy.begin(), vy.end());
 	n += 2;
 	m += 2;
 	vector<Edge> edges;
-	int cur_sect = 0, row = 1;
+	int curr_sect = 0, row = 1;
 	while (row < m) {
 		for (int i = 0; i < n - 2; i++) {
-			edges.push_back(Edge{vy[row] - vy[row - 1], cur_sect, cur_sect + 1});
-			cur_sect++;
+			edges.push_back(Edge{vy[row] - vy[row - 1], curr_sect, curr_sect + 1});
+			curr_sect++;
 		}
-		cur_sect++;
+		curr_sect++;
 		row++;
 	}
-	cur_sect = n - 1;
+	curr_sect = n - 1;
 	int col = 1;
 	while (col < n) {
-		int init = cur_sect;
+		const int init = curr_sect;
 		for (int i = 0; i < m - 2; i++) {
-			edges.push_back(Edge{vx[col] - vx[col - 1], cur_sect - (n - 1), cur_sect});
-			cur_sect += (n - 1);
+			edges.push_back(Edge{vx[col] - vx[col - 1], curr_sect - (n - 1), curr_sect});
+			curr_sect += (n - 1);
 		}
-		cur_sect = init + 1;
+		curr_sect = init + 1;
 		col++;
 	}
-	DSU dsu((n + 2) * (m + 2));
-	sort(edges.begin(), edges.end(),
-	     [](const Edge &t, const Edge &y) { return t.cost < y.cost; });
+	DisjointSets dsu((n + 2) * (m + 2));
+	sort(edges.begin(), edges.end(), comp);
 	long long ans = 0;
-	for (Edge &i : edges) {
-		if (dsu.unite(i.i, i.j)) { ans += i.cost; }
+	for (const auto &[cost, i, j] : edges) {
+		if (dsu.unite(i, j)) {
+			ans += cost;
+		}
 	}
 	cout << ans << endl;
 }
