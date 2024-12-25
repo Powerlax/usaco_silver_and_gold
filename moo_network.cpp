bf2b7ce14405f2b6fbc95435f2932b5e3c3b06c8
Powerlax <raovishruth@gmail.com>
2024-12-25 09:42:27 -0800

    update

--- moo_network.cpp
+++ moo_network.cpp
@@ -1,3 +1,28 @@
 //
 // Created by innav_z3e3dq9 on 11/24/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Edge {
+    pair<int, int> edge;
+
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<pair<int, int>> pos(n);
+    for (int i = 0; i < n; i++) {
+        cin >> pos[i].first >> pos[i].second;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            
+        }
+    }
+}
