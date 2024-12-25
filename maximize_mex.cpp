6b95afcd1de563eb722487a117d06bcd909f3e41
Powerlax <raovishruth@gmail.com>
2024-12-24 18:22:59 -0800

    update

--- maximize_mex.cpp
+++ maximize_mex.cpp
@@ -1,3 +1,36 @@
 //
 // Created by innav_z3e3dq9 on 12/17/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> arr(n);
+        map<int, int> frequency;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            frequency[arr[i]]++;
+        }
+        int ans = 0;
+        while (true) {
+            if (frequency[ans] < 1) {
+                break;
+            }
+            if (frequency[ans] > 1) {
+                frequency[ans + x] += frequency[ans] - 1;
+            }
+            ans++;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
