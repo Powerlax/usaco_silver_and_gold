5823f0ef452c335c424b088d766c046a10522e60
Powerlax <raovishruth@gmail.com>
2024-12-29 14:02:56 -0800

    update

--- uninteresting_number.cpp
+++ uninteresting_number.cpp
@@ -12,9 +12,35 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n;
+        string n;
         cin >> n;
-        
+        int twos = 0;
+        int threes = 0;
+        int sum = 0;
+        for (const char i : n) {
+            sum += i - '0';
+            if (i == '2') {
+                twos++;
+            } if (i == '3') {
+                threes++;
+            }
+        }
+        bool solved = false;
+        for (int i = 0; i <= twos && i < 9; i++) {
+            for (int j = 0; j <= threes && j < 9; j++) {
+                if ((sum + (i * 2) + (j * 6)) % 9 == 0) {
+                    cout << "YES" << endl;
+                    solved = true;
+                    break;
+                }
+            }
+            if (solved) {
+                break;
+            }
+        }
+        if (!solved) {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
