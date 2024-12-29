32169b3c42753b5300f0cc58197a5c5f887a01ef
Powerlax <raovishruth@gmail.com>
2024-12-29 14:03:07 -0800

    update

--- tree_matching.cpp
+++ tree_matching.cpp
@@ -1,3 +1,23 @@
 //
 // Created by innav_z3e3dq9 on 12/10/2024.
 //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        
+    }
+    return 0;
+}
