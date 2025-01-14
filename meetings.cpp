059d51d5ae869a0d3c3a5412d05e2ff05e1b7208
Powerlax <raovishruth@gmail.com>
2025-01-13 20:26:25 -0800

    update

--- meetings.cpp
+++ meetings.cpp
@@ -2,76 +2,83 @@
 // Created by innav_z3e3dq9 on 12/12/2024.
 //
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Cow {
-    int weight;
-    int position;
-    int direction;
-};
+int n, l;
+vector<int> w, x, d;
+
+bool comp(const int i, const int j) {
+    return x[i] < x[j];
+}
+
+int getTime() {
+    vector<int> left, right;
+    for (int i = 0; i < n; i++) {
+        if (d[i] == -1) {
+            left.push_back(x[i]);
+        } else {
+            right.push_back(x[i]);
+        }
+    }
+    vector<pair<int, int>> v;
+    for (int i = 0; i < left.size(); i++) {
+        v.emplace_back(left[i], w[i]);
+    }
+    for (int i = 0; i < right.size(); i++) {
+        v.emplace_back(l - right[i], w[left.size() + i]);
+    }
+    sort(v.begin(), v.end());
+    int tot = 0;
+    for (auto [fst, snd] : v) {
+        tot += snd;
+    }
+    for (auto [fst, snd] : v) {
+        tot -= 2 * snd;
+        if (tot <= 0) {
+            return fst;
+        }
+    }
+    return 0;
+}
 
 int main() {
-    int N, L;
-    cin >> N >> L;
-    freopen("input.txt", "r", cin.get());
-
-    vector<Cow> cows(N);
-    vector<pair<int, int>> events; // {time, event type (-1 for left, +1 for right)}
-    int totalWeight = 0;
-
-    for (int i = 0; i < N; i++) {
-        cin >> cows[i].weight >> cows[i].position >> cows[i].direction;
-        totalWeight += cows[i].weight;
-        if (cows[i].direction == -1) {
-            events.push_back({cows[i].position, -1});
-        } else {
-            events.push_back({L - cows[i].position, 1});
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    freopen("meetings.in", "r", stdin);
+    freopen("meetings.out", "w", stdout);
+    cin >> n >> l;
+    w.resize(n);
+    x.resize(n);
+    d.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> w[i] >> x[i] >> d[i];
     }
-
-    sort(events.begin(), events.end());
-
-    int halfWeight = (totalWeight + 1) / 2;
-    int stoppedWeight = 0;
-    int T = 0;
-
-    for (const auto& event : events) {
-        stoppedWeight += event.second == -1 ? cows[event.second].weight : cows[event.second].weight;
-        if (stoppedWeight >= halfWeight) {
-            T = event.first;
-            break;
-        }
+    vector<int> speeds(n);
+    iota(speeds.begin(), speeds.end(), 0);
+    sort(speeds.begin(), speeds.end(), comp);
+    vector<int> speed_w, speed_x, speed_d;
+    for (const int i : speeds) {
+        speed_w.push_back(w[i]);
+        speed_x.push_back(x[i]);
+        speed_d.push_back(d[i]);
     }
-
-    // Calculate number of meetings
-    vector<pair<int, int>> positions;
-    for (int i = 0; i < N; i++) {
-        positions.push_back({cows[i].position, cows[i].direction});
-    }
-
-    sort(positions.begin(), positions.end());
-
-    int meetings = 0;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = i + 1; j < N; j++) {
-            if (positions[i].second == 1 && positions[j].second == -1) {
-                double meetTime = (positions[j].first - positions[i].first) / 2.0;
-                if (meetTime <= T) {
-                    cout << "hi" << endl;
-                    meetings++;
-                }
+    swap(w, speed_w);
+    swap(x, speed_x);
+    swap(d, speed_d);
+    const long int t = getTime();
+    queue<int> frontier;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (d[i] == -1) {
+            while (!frontier.empty() && frontier.front() + 2 * t < x[i]) {
+                frontier.pop();
             }
+            ans += static_cast<int>(frontier.size());
+        } else {
+            frontier.push(x[i]);
         }
     }
-
-    cout << meetings << endl;
-
-    return 0;
+    cout << ans << endl;
 }
