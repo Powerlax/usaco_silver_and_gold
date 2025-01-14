ba93d58814416f5bb5bbc259e8a8ffb67bc7dcd1
Powerlax <raovishruth@gmail.com>
2025-01-13 20:25:57 -0800

    update

--- email_filing.cpp
+++ email_filing.cpp
@@ -2,49 +2,90 @@
 // Created by innav_z3e3dq9 on 12/13/2024.
 //
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool canFileEmails(int M, int N, int K, const vector<int>& folders) {
-    int folderStart = 1;
-    int emailsProcessed = 0;
-    while (emailsProcessed < N) {
-        int targetFolder = folders[emailsProcessed];
-        while (targetFolder < folderStart || targetFolder >= folderStart + K) {
-            folderStart++;
-            if (folderStart > M - K + 1) {
-                return false;
-            }
-        }
-        emailsProcessed++;
-    }
+vector<int> folders;
 
-    return true;
+bool comp(const int i, const int j) {
+    return folders[i] > folders[j];
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int T;
-    cin >> T;
-    while (T--) {
-        int M, N, K;
-        cin >> M >> N >> K;
-
-        vector<int> folders(N);
-        for (int i = 0; i < N; ++i) {
+    int t;
+    cin >> t;
+    while (t--) {
+        int m, n, k;
+        cin >> m >> n >> k;
+        folders.resize(n);
+        vector<int> remaining(m);
+        for (int i = 0; i < n; i++) {
             cin >> folders[i];
+            folders[i]--;
+            remaining[folders[i]]++;
         }
-
-        if (canFileEmails(M, N, K, folders)) {
-            cout << "YES\n";
-        } else {
-            cout << "NO\n";
+        int curr_folder = 0, l = 0, r = k - 1;
+        vector<bool> ok(n);
+        priority_queue<int, vector<int>, decltype(comp)*> pq{comp};
+        for (int i = 0; i < k; i++) {
+            pq.push(i);
+        }
+        while (r < n - 1 && curr_folder < m) {
+            if (remaining[curr_folder] == 0) {
+                curr_folder++;
+            } else if (!pq.empty() && folders[pq.top()] < curr_folder + k) {
+                const int curr = pq.top();
+                pq.pop();
+                if (curr >= l) {
+                    ok[curr] = true;
+                    remaining[folders[curr]]--;
+                    r++;
+                    pq.push(r);
+                }
+            } else {
+                while(l < n && ok[l]) {
+                    l++;
+                }
+                l++;
+                r++;
+                pq.push(r);
+            }
+        }
+        bool impossible = false;
+        while (curr_folder < m) {
+            if (remaining[curr_folder] == 0) {
+                curr_folder++;
+            } else {
+                if (pq.empty()) {
+                    cout << "NO" << endl;
+                    impossible = true;
+                    break;
+                }
+                const int curr = pq.top();
+                pq.pop();
+                if (curr >= l && !ok[curr]) {
+                    if (folders[curr] >= curr_folder + k) {
+                        cout << "NO" << endl;
+                        impossible = true;
+                        break;
+                    }
+                    ok[curr] = true;
+                    remaining[folders[curr]]--;
+                    while(l > 0) {
+                        l--;
+                        if (!ok[l]) {
+                            pq.push(l);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if (!impossible) {
+            cout << "YES" << endl;
         }
     }
-
     return 0;
 }
-
